8e75627d72c9efa61f677af2647b7913a69e7b79
uk <ukkarthi07@gmail.com>
2024-01-30 18:23:33 +0530

    Todo Planner Added

---
+++ To Do Planner/Backend/Database/Connection.js
@@ -0,0 +1,28 @@
+const {Pool} = require("pg");
+
+let mainConnection;
+
+const credentials={
+    user:"postgres",
+    host:"localhost",
+    database:"ToDoPlanner",
+    password:"123",
+    port:"5432"
+};
+
+const connect = ()=>{
+ mainConnection= new Pool(credentials);
+};
+
+const getConnection=()=>{
+ if(mainConnection){
+    return mainConnection;
+ }
+ else{
+    connect();
+ }
+ return mainConnection;
+};
+module.exports={
+   instanceofDB:getConnection
+};
