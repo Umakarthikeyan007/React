8af2a1fb850dd5a3e4ae22d758bb1780667c0a7c
uk <ukkarthi07@gmail.com>
2024-01-30 18:38:55 +0530

    Removed

--- Employee CRUD without JWT/Backend/Router/index.js
+++
@@ -1,5 +0,0 @@
-const express=require("express");
-const Router =require("./User");
-const router= express.Router();
-router.use('/Users',Router.userRouter);
-module.exports=router
--- Employee CRUD without JWT/Backend/Utils/formatDate.js
+++
@@ -1,9 +0,0 @@
-function alterDataForDate(data){
-    for(let index=0;index<data.length;index++){
-        const date=new Date(data[index].dob);
-        data[index].dob=date.getDate()+"-"+(date.getMonth()+1)+"-"+date.getFullYear();
-    }
-    return data;
-}
-
-module.exports={alterDataForDate}
